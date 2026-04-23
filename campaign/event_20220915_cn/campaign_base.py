dff9ef0fb46348094989654bd913dc4ee8690264
茗 <152146621+wess09@users.noreply.github.com>
2026-04-23 22:31:44 +0800

    Fix bugs and update event handling for dual events (活动更新来自guoh064/AzurLaneAutoScript)
    * Fix: Random background hits EXP_INFO_B in event_20220915_cn chapter A
    
    * Fix: spare domain of git-over-cdn
    
    * Upd: opsi shop asset templates (#5617)
    
    * Fix: always add items, even if last item list contains unknown items
    
    * Add: auto change state of options (default select and single means state)
    
    * Add: distangle Gemsfarming.Campaign.Event from Event.Campaign.Event for multi event
    
    * Upd: [JP] asset MISSION_UNFINISH
    
    * Add: event entrance for dual event 20201126/20260417
    
    ---------
    
    Co-authored-by: LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
    Co-authored-by: LmeSzinc <lmeszincsales@gmail.com>
    Co-authored-by: wess09 <ZhangMusan@noreply.gitcode.com>
    Co-authored-by: guoh064 <50830808+guoh064@users.noreply.github.com>

--- campaign/event_20220915_cn/campaign_base.py
+++ campaign/event_20220915_cn/campaign_base.py
@@ -14,7 +14,7 @@ class CampaignBase(CampaignBase_):
         ) > self.MAP_ENEMY_SEARCHING_OVERLAY_TRANSPARENCY_THRESHOLD
 
     def handle_exp_info(self):
-        # Skip handling EXP_INFO on event page
+        # Random background hits EXP_INFO_B
         if self.ui_page_appear(page_event):
             return False
         return super().handle_exp_info()
